740a5b2818dc48460b58b368398a953cd71bd705
serj <sergejtustin601@gmail.com>
2025-02-26 22:48:04 +0300

    time sort function

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from src.widget import get_date, mask_account_card
 
 from src.processing import filter_by_state
 
+from src.processing import sort_by_date
 
 # Проверка работы виджета
 print(mask_account_card("Maestro 1596837868705199"))
@@ -17,3 +18,5 @@ print(get_date("2024-03-11T02:26:18.671407"))
 # Проверка функции в новом модуле src/processing
 print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
+# Проверка функции сортировки по времени
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
--- src/processing.py
+++ src/processing.py
@@ -6,3 +6,6 @@ def filter_by_state(transactions: list[dict[str, Union[str | int]]], state = "EX
         if user_info["state"] == state:
             state_transactions.append(user_info)
     return state_transactions
+
+def sort_by_date(transactions: list[dict[str, Union[str | int]]], reverse: bool = True) -> list[dict[str, Union[str | int]]]:
+    return sorted(transactions, key=lambda x: x["date"], reverse = reverse)
