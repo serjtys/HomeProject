0f5d03cefe40d0edaa82900d0e91422d8baa1291
serj <sergejtustin601@gmail.com>
2025-04-13 00:25:32 +0300

    add docstring

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import os
 
 
 def load_transactions(file_path):
+    ''' Функция чтения json файлов, принимает в качестве аргумента пусть к файлу'''
     if not os.path.isfile(file_path):
         return []
     with open(file_path, "r", encoding="utf-8") as file:
